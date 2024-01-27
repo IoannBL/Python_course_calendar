64e237923bc9f157f597b85d2c99ff73f7217b6e
Ioann_B <git@github.com:IoannBL/DZ_2.git>
2024-01-27 18:36:19 +0300

    Добавлен класс NotificationManager

--- main.py
+++ main.py
@@ -1,16 +1,1 @@
 
-import flet as ft
-import time
-def window(page: ft.Page):
-    t = ft.Text(value="Hello World")
-    page.controls.append(t)
-    
-    
-    
-    
-    for i in range(1000):
-        time.sleep(1)
-        t.value = f"hello world {i}"
-        page.update()
-
-ft.app(target=window)
